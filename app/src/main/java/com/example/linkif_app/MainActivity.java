d2b3c70d1a8ec0334423ae70a5be8fb10cf6352f
gabriel.miranda <gabriel.alan32@gmail.com>
2021-04-27 15:16:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/linkif_app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.linkif_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.linkif_app.Activity.Login;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null){
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
